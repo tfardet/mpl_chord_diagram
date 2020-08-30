6dfb8cca85283e1011a6344d09bfd514d56c779e
Silmathoron <Silmathoron@users.noreply.github.com>
2020-08-30 14:37:15 +0200

    included all PRs + started refactoring

--- matplotlib-chord.py
+++ matplotlib-chord.py
@@ -1,32 +1,49 @@
-###################
-# chord diagram
-import matplotlib.pyplot as plt
-from matplotlib.path import Path
+"""
+Tools to draw a chord diagram in python
+
+License: MIT
+
+Contributors:
+* original author is fengwangPhysics (https://github.com/fengwangPhysics)
+* improved arcs by cy1110 (https://github.com/cy1110)
+* colormap support by pakitochus (https://github.com/pakitochus)
+* refactoring by Silmathoron
+"""
+
+from collections.abc import Sequence
+
 import matplotlib.patches as patches
+from matplotlib.path import Path
+from matplotlib.colors import ColorConverter
 
 import numpy as np
 
 LW = 0.3
 
+
 def polar2xy(r, theta):
     return np.array([r*np.cos(theta), r*np.sin(theta)])
 
-def hex2rgb(c):
-    return np.array(int(c[i:i+2], 16)/256.0 for i in (1, 3 ,5))
 
-def IdeogramArc(start=0, end=60, radius=1.0, width=0.2, ax=None, color=(1,0,0)):
-    # start, end should be in [0, 360)
+def initial_path(start, end, radius, width):
+    ''' First 16 vertices and 15 instructions are the same for everyone '''
     if start > end:
         start, end = end, start
+
     start *= np.pi/180.
-    end *= np.pi/180.
+    end   *= np.pi/180.
+
     # optimal distance to the control points
-    # https://stackoverflow.com/questions/1734745/how-to-create-circle-with-b%C3%A9zier-curves
+    # https://stackoverflow.com/questions/1734745/
+    # how-to-create-circle-with-b%C3%A9zier-curves
+    # use 16-vertex curves (4 quadratic Beziers which accounts for worst case
+    # scenario of 360 degrees)
     inner = radius*(1-width)
-    opt = 4./3. * np.tan((end-start)/ 16.) * radius #16-vertex curves (4 quadratic Beziers which accounts for worst case scenario of 360 degrees)
+    opt   = 4./3. * np.tan((end-start)/ 16.) * radius
     inter1 = start*(3./4.)+end*(1./4.)
     inter2 = start*(2./4.)+end*(2./4.)
     inter3 = start*(1./4.)+end*(3./4.)
+
     verts = [
         polar2xy(radius, start),
         polar2xy(radius, start) + polar2xy(opt, start+0.5*np.pi),
@@ -43,7 +60,60 @@ def IdeogramArc(start=0, end=60, radius=1.0, width=0.2, ax=None, color=(1,0,0)):
         polar2xy(radius, inter3),
         polar2xy(radius, inter3) + polar2xy(opt, inter3+0.5*np.pi),
         polar2xy(radius, end) + polar2xy(opt, end-0.5*np.pi),
-        polar2xy(radius, end),
+        polar2xy(radius, end)
+    ]
+
+    codes = [
+        Path.MOVETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+    ]
+
+    return start, end, verts, codes
+
+
+def IdeogramArc(start, end, radius=1., width=0.2, color="r", alpha=0.7, ax=None):
+    '''
+    Draw an arc between two regions of the chord diagram.
+
+    Parameters
+    ----------
+    start : float (degree in 0, 360)
+        Starting degree.
+    end : float (degree in 0, 360)
+        Final degree.
+    radius : float, optional (default: 1)
+    width : float, optional (default: 0.2)
+    ax : matplotlib axis, optional (default: not plot)
+    color : valid matplotlib color, optional (default: "r")
+
+    Returns
+    -------
+    verts, codes : lists
+        Vertices and path instructions to draw the shape.
+    '''
+    start, end, verts, codes = initial_path(start, end, radius, width)
+
+    opt    = 4./3. * np.tan((end-start)/ 16.) * radius
+    inner  = radius*(1-width)
+    inter1 = start*(3./4.)+end*(1./4.)
+    inter2 = start*(2./4.)+end*(2./4.)
+    inter3 = start*(1./4.)+end*(3./4.)
+
+    verts += [
         polar2xy(inner, end),
         polar2xy(inner, end) + polar2xy(opt*(1-width), end-0.5*np.pi),
         polar2xy(inner, inter3) + polar2xy(opt*(1-width), inter3+0.5*np.pi),
@@ -61,218 +131,113 @@ def IdeogramArc(start=0, end=60, radius=1.0, width=0.2, ax=None, color=(1,0,0)):
         polar2xy(inner, start) + polar2xy(opt*(1-width), start+0.5*np.pi),
         polar2xy(inner, start),
         polar2xy(radius, start),
-        ]
+    ]
 
-    codes = [Path.MOVETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CLOSEPOLY,
-             ]
+    codes += [
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CLOSEPOLY,
+    ]
 
-    if ax == None:
-        return verts, codes
-    else:
-        path = Path(verts, codes)
-        patch = patches.PathPatch(path, facecolor=tuple(color)+(0.5,), edgecolor=tuple(color)+(0.4,), lw=LW)
+    if ax is not None:
+        path  = Path(verts, codes)
+        patch = patches.PathPatch(path, facecolor=tuple(color) + (alpha,),
+                                  edgecolor=tuple(color) + (alpha,), lw=LW)
         ax.add_patch(patch)
 
+    return verts, codes
 
-def ChordArc(start1=0, end1=60, start2=180, end2=240, radius=1.0, chordwidth=0.7, ax=None, color=(1,0,0)):
-    # start, end should be in [0, 360)
-    if start1 > end1:
-        start1, end1 = end1, start1
-    if start2 > end2:
-        start2, end2 = end2, start2
-    start1 *= np.pi/180.
-    end1 *= np.pi/180.
-    start2 *= np.pi/180.
-    end2 *= np.pi/180.
-    opt1 = 4./3. * np.tan((end1-start1)/ 16.) * radius #16-vertex curves (4 quadratic Beziers which accounts for worst case scenario of 360 degrees)
-    opt2 = 4./3. * np.tan((end2-start2)/ 16.) * radius #16-vertex curves (4 quadratic Beziers which accounts for worst case scenario of 360 degrees)
+
+def ChordArc(start1, end1, start2, end2, radius=1.0, chordwidth=0.7, color=(1,0,0), alpha=0.7, ax=None):
+    start1, end1, verts, codes = initial_path(start1, end1, radius, chordwidth)
+
+    start2, end2, verts2, _ = initial_path(start2, end2, radius, chordwidth)
+
     rchord = radius * (1-chordwidth)
-    inter11 = start1*(3./4.)+end1*(1./4.)
-    inter12 = start1*(2./4.)+end1*(2./4.)
-    inter13 = start1*(1./4.)+end1*(3./4.)
-    inter21 = start2*(3./4.)+end2*(1./4.)
-    inter22 = start2*(2./4.)+end2*(2./4.)
-    inter23 = start2*(1./4.)+end2*(3./4.)
-    verts = [
-        polar2xy(radius, start1),
-        polar2xy(radius, start1) + polar2xy(opt1, start1+0.5*np.pi),
-        polar2xy(radius, inter11) + polar2xy(opt1, inter11-0.5*np.pi),
-        polar2xy(radius, inter11),
-        polar2xy(radius, inter11),
-        polar2xy(radius, inter11) + polar2xy(opt1, inter11+0.5*np.pi),
-        polar2xy(radius, inter12) + polar2xy(opt1, inter12-0.5*np.pi),
-        polar2xy(radius, inter12),
-        polar2xy(radius, inter12),
-        polar2xy(radius, inter12) + polar2xy(opt1, inter12+0.5*np.pi),
-        polar2xy(radius, inter13) + polar2xy(opt1, inter13-0.5*np.pi),
-        polar2xy(radius, inter13),
-        polar2xy(radius, inter13),
-        polar2xy(radius, inter13) + polar2xy(opt1, inter13+0.5*np.pi),
-        polar2xy(radius, end1) + polar2xy(opt1, end1-0.5*np.pi),
-        polar2xy(radius, end1),
-        polar2xy(rchord, end1),
-        polar2xy(rchord, start2),
-        polar2xy(radius, start2),
-        polar2xy(radius, start2) + polar2xy(opt2, start2+0.5*np.pi),
-        polar2xy(radius, inter21) + polar2xy(opt2, inter21-0.5*np.pi),
-        polar2xy(radius, inter21),
-        polar2xy(radius, inter21),
-        polar2xy(radius, inter21) + polar2xy(opt2, inter21+0.5*np.pi),
-        polar2xy(radius, inter22) + polar2xy(opt2, inter22-0.5*np.pi),
-        polar2xy(radius, inter22),
-        polar2xy(radius, inter22),
-        polar2xy(radius, inter22) + polar2xy(opt2, inter22+0.5*np.pi),
-        polar2xy(radius, inter23) + polar2xy(opt2, inter23-0.5*np.pi),
-        polar2xy(radius, inter23),
-        polar2xy(radius, inter23),
-        polar2xy(radius, inter23) + polar2xy(opt2, inter23+0.5*np.pi),
-        polar2xy(radius, end2) + polar2xy(opt2, end2-0.5*np.pi),
-        polar2xy(radius, end2),
+
+    verts += [polar2xy(rchord, end1), polar2xy(rchord, start2)] + verts2
+
+    verts += [
         polar2xy(rchord, end2),
         polar2xy(rchord, start1),
         polar2xy(radius, start1),
-        ]
+    ]
 
-    codes = [Path.MOVETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             ]
+    codes += [
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.LINETO,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+    ]
 
-    if ax == None:
-        return verts, codes
-    else:
+    if ax is not None:
         path = Path(verts, codes)
-        patch = patches.PathPatch(path, facecolor=tuple(color)+(0.5,), edgecolor=tuple(color)+(0.4,), lw=LW)
+        patch = patches.PathPatch(path, facecolor=tuple(color)+(alpha,),
+                                  edgecolor=tuple(color)+(alpha,), lw=LW)
         ax.add_patch(patch)
 
-def selfChordArc(start=0, end=60, radius=1.0, chordwidth=0.7, ax=None, color=(1,0,0)):
-    # start, end should be in [0, 360)
-    if start > end:
-        start, end = end, start
-    start *= np.pi/180.
-    end *= np.pi/180.
-    opt = 4./3. * np.tan((end-start)/ 16.) * radius #16-vertex curves (4 quadratic Beziers which accounts for worst case scenario of 360 degrees)
-    inter1 = start*(3./4.)+end*(1./4.)
-    inter2 = start*(2./4.)+end*(2./4.)
-    inter3 = start*(1./4.)+end*(3./4.)
+    return verts, codes
+
+
+def selfChordArc(start=0, end=60, radius=1.0, chordwidth=0.7, color=(1,0,0), alpha=0.7, ax=None):
+    start, end, verts, codes = initial_path(start, end, radius, chordwidth)
+    
     rchord = radius * (1-chordwidth)
-    verts = [
-        polar2xy(radius, start),
-        polar2xy(radius, start) + polar2xy(opt, start+0.5*np.pi),
-        polar2xy(radius, inter1) + polar2xy(opt, inter1-0.5*np.pi),
-        polar2xy(radius, inter1),
-        polar2xy(radius, inter1),
-        polar2xy(radius, inter1) + polar2xy(opt, inter1+0.5*np.pi),
-        polar2xy(radius, inter2) + polar2xy(opt, inter2-0.5*np.pi),
-        polar2xy(radius, inter2),
-        polar2xy(radius, inter2),
-        polar2xy(radius, inter2) + polar2xy(opt, inter2+0.5*np.pi),
-        polar2xy(radius, inter3) + polar2xy(opt, inter3-0.5*np.pi),
-        polar2xy(radius, inter3),
-        polar2xy(radius, inter3),
-        polar2xy(radius, inter3) + polar2xy(opt, inter3+0.5*np.pi),
-        polar2xy(radius, end) + polar2xy(opt, end-0.5*np.pi),
-        polar2xy(radius, end),
+
+    verts += [
         polar2xy(rchord, end),
         polar2xy(rchord, start),
         polar2xy(radius, start),
-        ]
+    ]
 
-    codes = [Path.MOVETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.LINETO,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             Path.CURVE4,
-             ]
+    codes += [
+        Path.CURVE4,
+        Path.CURVE4,
+        Path.CURVE4,
+    ]
 
-    if ax == None:
-        return verts, codes
-    else:
+    if ax is not None:
         path = Path(verts, codes)
-        patch = patches.PathPatch(path, facecolor=tuple(color)+(0.5,), edgecolor=tuple(color)+(0.4,), lw=LW)
+        patch = patches.PathPatch(path, facecolor=tuple(color)+(alpha,),
+                                  edgecolor=tuple(color)+(alpha,), lw=LW)
         ax.add_patch(patch)
-        
-def chordDiagram(X, ax, colors=None, width=0.1, pad=2, chordwidth=0.7):
+
+    return verts, codes
+
+
+def chordDiagram(X, ax, width=0.1, pad=2, chordwidth=0.7, colors=None,
+                 cmap=None, alpha=0.7):
     """Plot a chord diagram
 
     Parameters
@@ -281,8 +246,10 @@ def chordDiagram(X, ax, colors=None, width=0.1, pad=2, chordwidth=0.7):
         flux data, X[i, j] is the flux from i to j
     ax :
         matplotlib `axes` to show the plot
-    colors : optional
-        list of user defined colors in rgb format or valid matplotlib colormap string. Use function hex2rgb() to convert hex color to rgb color. Default: d3.js category10
+    colors : list, optional (default: from `cmap`)
+        List of user defined colors or floats.
+    cmap : str or colormap object (default: viridis)
+        Colormap to use.
     width : optional
         width/thickness of the ideogram arc
     pad : optional
@@ -290,71 +257,100 @@ def chordDiagram(X, ax, colors=None, width=0.1, pad=2, chordwidth=0.7):
     chordwidth : optional
         position of the control points for the chords, controlling the shape of the chords
     """
+    import matplotlib.pyplot as plt
+
     # X[i, j]:  i -> j
+    num_nodes = len(X)
+
     x = X.sum(axis = 1) # sum over rows
     ax.set_xlim(-1.1, 1.1)
     ax.set_ylim(-1.1, 1.1)
     
-    # First, set default to category10 color list
+    # First, set default to viridis color list
     if colors is None:
-        colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
-                  '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-        colors = [hex2rgb(colors[i]) for i in range(len(x))]
-    
-    # Then, if color is default or a specified list, assert correct length
-    if type(colors) is list:
-        # use the list of colors (and `assert len(colors)==len(x)`)
-        assert len(colors)>=len(x)
-    # If it was a string, compute colormap. 
-    elif type(colors) is str:
-        cm = plt.get_cmap(colors) 
-        colors = cm(np.linspace(0,1,len(x)))[:,:3]
-        
+        colors = np.linspace(0, 1, num_nodes)
+
+    if cmap is None:
+        cmap = "viridis"
+
+    if isinstance(colors, (Sequence, np.ndarray)):
+        assert len(colors) == num_nodes, "One color per node is required."
+
+        # check color type
+        first_color = colors[0]
+
+        if isinstance(first_color, (int, float, np.integer)):
+            cm = plt.get_cmap(cmap)
+            colors = cm(colors)[:, :3]
+        else:
+            colors = [ColorConverter.to_rgb(c) for c in colors]
+    else:
+        raise ValueError("`colors` should be a list.")
 
     # find position for each start and end
-    y = x/np.sum(x).astype(float) * (360 - pad*len(x))
+    y = x / np.sum(x).astype(float) * (360 - pad*len(x))
 
     pos = {}
     arc = []
     nodePos = []
     start = 0
+
     for i in range(len(x)):
         end = start + y[i]
         arc.append((start, end))
         angle = 0.5*(start+end)
-        #print(start, end, angle)
+
         if -30 <= angle <= 210:
             angle -= 90
         else:
             angle -= 270
-        nodePos.append(tuple(polar2xy(1.1, 0.5*(start+end)*np.pi/180.)) + (angle,))
-        z = (X[i, :]/x[i].astype(float)) * (end - start)
+
+        nodePos.append(
+            tuple(polar2xy(1.1, 0.5*(start + end)*np.pi/180.)) + (angle,))
+
+        z = (X[i, :] / x[i].astype(float)) * (end - start)
+
         ids = np.argsort(z)
+
         z0 = start
+
         for j in ids:
             pos[(i, j)] = (z0, z0+z[j])
             z0 += z[j]
+
         start = end + pad
 
     for i in range(len(x)):
         start, end = arc[i]
-        IdeogramArc(start=start, end=end, radius=1.0, ax=ax, color=colors[i], width=width)
+
+        IdeogramArc(start=start, end=end, radius=1.0, color=colors[i],
+                    width=width, alpha=alpha, ax=ax)
+
         start, end = pos[(i,i)]
-        selfChordArc(start, end, radius=1.-width, color=colors[i], chordwidth=chordwidth*0.7, ax=ax)
+
+        selfChordArc(start, end, radius=1 - width, chordwidth=chordwidth*0.7,
+                     color=colors[i], alpha=alpha, ax=ax)
+
         for j in range(i):
             color = colors[i]
+
             if X[i, j] > X[j, i]:
                 color = colors[j]
+
             start1, end1 = pos[(i,j)]
             start2, end2 = pos[(j,i)]
-            ChordArc(start1, end1, start2, end2,
-                     radius=1.-width, color=colors[i], chordwidth=chordwidth, ax=ax)
 
-    #print(nodePos)
+            ChordArc(start1, end1, start2, end2, radius=1 - width,
+                     chordwidth=chordwidth, color=colors[i], alpha=alpha,
+                     ax=ax)
+
     return nodePos
+
 
 ##################################
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     fig = plt.figure(figsize=(6,6))
     flux = np.array([[11975,  5871, 8916, 2868],
       [ 1951, 10048, 2060, 6171],
@@ -372,7 +368,5 @@ if __name__ == "__main__":
     for i in range(4):
         ax.text(nodePos[i][0], nodePos[i][1], nodes[i], rotation=nodePos[i][2], **prop)
 
-    plt.savefig("example.png", dpi=600,
-            transparent=True,
-            bbox_inches='tight', pad_inches=0.02)
+    plt.show()
 
