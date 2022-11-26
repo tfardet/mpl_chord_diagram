74ac5981949fcdcee7a35e6ac6640ac72521882e
Tanguy Fardet <tfardet@noreply.codeberg.org>
2022-11-26 03:22:01 +0000

    Version 0.4.1 with improved setup.py (#44)
    I found a much simpler and cleaner way to make a pypi project with a flat repository structure (was embarassingly simple), so I cleaned up the setup.py
    
    Co-authored-by: Tanguy Fardet <tanguyfardet@protonmail.com>
    Reviewed-on: https://codeberg.org/tfardet/mpl_chord_diagram/pulls/44

--- setup.py
+++ setup.py
@@ -1,42 +1,11 @@
-import os, errno
 from setuptools import setup, find_packages
-from shutil import rmtree, copy2
-
-
-# create mpl_chord_diagram directory and move important files
-setup_dir = os.path.abspath(os.path.dirname(__file__))
-directory = os.path.join(setup_dir, 'mpl_chord_diagram')
-
-move = (
-    '__init__.py',
-    '_info.py',
-    'LICENSE',
-    'chord_diagram.py',
-    'gradient.py',
-    'utilities.py',
-    'example.py',
-)
-
-
-# Layout changes between git repo (flat) and Pypi (hierarchical) so check if
-# flat layout files exist. If not, then all files are already in the subfolder
-
-dir_preexists = False
-
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-    for fname in move:
-        copy2(fname, directory)
-else:
-    dir_preexists = True
 
 
 # Get version information, recommended here
 # https://packaging.python.org/guides/single-sourcing-package-version
 info = {}
 
-with open(os.path.join(directory, "_info.py")) as fp:
+with open("_info.py") as fp:
     exec(fp.read(), info)
 
 
@@ -48,39 +17,34 @@ It provides tunable parameters such as arc sorting based on size or distance,
 and supports gradients to better visualize source and target regions.
 '''
 
-try:
-    setup(
-        name='mpl_chord_diagram',
-        version=info['__version__'],
-        description='Python module to plot chord diagrams with matplotlib.',
-        package_dir={'': '.'},
-        packages=find_packages('.'),
+setup(
+    name='mpl_chord_diagram',
+    version=info['__version__'],
+    description='Python module to plot chord diagrams with matplotlib.',
+    package_dir={'mpl_chord_diagram': '.'},
+    packages=['mpl_chord_diagram'],
 
-        # Include the non python files:
-        package_data={'': ['LICENSE', '*.md']},
+    # Include the non python files:
+    package_data={'': ['LICENSE', '*.md']},
 
-        # Requirements
-        install_requires=['numpy', 'scipy', 'matplotlib'],
+    # Requirements
+    install_requires=['numpy', 'scipy', 'matplotlib'],
 
-        # Metadata
-        url='https://codeberg.org/tfardet/mpl_chord_diagram',
-        author='Tanguy Fardet',
-        author_email='tanguy.fardet@tuebingen.mpg.de',
-        license='MIT',
-        keywords='chord diagram matplotlib plotting',
-        long_description=long_descr,
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Programming Language :: Python :: 3',
-            'Operating System :: OS Independent',
-            'Topic :: Scientific/Engineering :: Visualization',
-            'Framework :: Matplotlib',
-        ]
-    )
-finally:
-    # only useful for package generation from git repo
-    if not dir_preexists:
-        rmtree(directory, ignore_errors=True)
+    # Metadata
+    url='https://codeberg.org/tfardet/mpl_chord_diagram',
+    author=info['__author__'],
+    author_email=info['__email__'],
+    license='MIT',
+    keywords='chord diagram matplotlib plotting',
+    long_description=long_descr,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Framework :: Matplotlib',
+    ]
+)
